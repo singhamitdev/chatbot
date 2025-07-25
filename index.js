70baeb8f03fc455e9c28399a516a94a800f501f4
Amit Singh <93870725+singhamitdev@users.noreply.github.com>
2025-07-25 14:03:42 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
+import App from "./App";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "../src/styles/global.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+      <ToastContainer position="top-right" autoClose={2000} />
+    </PersistGate>
+  </Provider>
+);
